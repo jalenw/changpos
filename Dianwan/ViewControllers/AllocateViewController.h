f4067d41fd921c276fc4b28d47f268be2d8c7582
jalenw <154812363@qq.com>
2019-01-17 17:09:01 +0800

    调拨

---
+++ Dianwan/ViewControllers/AllocateViewController.h
@@ -0,0 +1,24 @@
+//
+//  AllocateViewController.h
+//  Dianwan
+//
+//  Created by 黄哲麟 on 2019/1/17.
+//  Copyright © 2019年 intexh. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AllocateViewController : BaseViewController
+@property (weak, nonatomic) IBOutlet UITextField *productTf;
+@property (weak, nonatomic) IBOutlet UITextField *snTf;
+@property (weak, nonatomic) IBOutlet UITextField *totalTf;
+@property (weak, nonatomic) IBOutlet UITextField *partnerTf;
+- (IBAction)productAct:(UIButton *)sender;
+- (IBAction)snAct:(UIButton *)sender;
+- (IBAction)partnerAct:(UIButton *)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
